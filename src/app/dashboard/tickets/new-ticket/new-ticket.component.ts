8eb3c6a61385c8e5699bd5ba51279587970a85d7
Irena <irenva101@gmail.com>
2024-07-31 15:45:41 +0200

    template forms, live update on the GUI using ngOnInit

--- src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
+++ src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
@@ -1,14 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { ControlComponent } from "../../../shared/control/control.component";
+import { ButtonComponent } from '../../../shared/button/button.component';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-new-ticket',
   standalone: true,
-  imports: [ControlComponent],
+  imports: [ControlComponent, ButtonComponent, FormsModule],
   templateUrl: './new-ticket.component.html',
   styleUrl: './new-ticket.component.css',
   
 })
 export class NewTicketComponent {
-
+  @ViewChild('form') form?: ElementRef<HTMLFormElement>;
+  onSubmit(title: string, ticketText: String, form:HTMLFormElement){
+    console.log(title);
+    console.log(ticketText);
+    this.form?.nativeElement.reset();
+  }
 }
